b6271905e782edda0fb190f02ab49066b368c13b
Robert Scholl <robertscholl.private@gmail.com>
2019-06-05 04:07:08 +0200

    Added more options

--- CreateCMakeProject.sh
+++ CreateCMakeProject.sh
@@ -17,7 +17,10 @@ function usage () {
 }
 
 PROJECT_NAME="";
-USE_GIT=false;
+CREATE_CMAKE_BOILERPLATE=false;
+CREATE_DOCUMENTATION=false;
+CREATE_GIT_REPOSITORY=false;
+CMAKE_VERSION=$(cmake --version | awk '{print $3}' | grep -m1 "");
 
 #
 # Parameter Parsing
@@ -26,25 +29,32 @@ USE_GIT=false;
 # check if there are parameters
 if [[ $# -ge 1 ]]; then
     PROJECT_NAME=$1;
-    # iterate over other parameters
+    shift;
+    # iterate over parameters and set flags for project creation
     while [ "$1" != "" ]; do
         case $1 in
-            -g | --git )            USE_GIT=true;
+            -c | --cmake )          CREATE_CMAKE_BOILERPLATE=true;
                                     ;;
-            -h | --help )           usage
-                                    exit
+            -cv | --cmake-version   shift;
+                                    CMAKE_VERSION=$1;
                                     ;;
-            * )                     if [[ $PROJECT_NAME != $1 ]]; then
-                                        usage
-                                        exit 1
-                                    fi
+            -d | --docs )           CREATE_DOCUMENTATION=true;
+                                    ;;
+            -g | --git )            CREATE_GIT_REPOSITORY=true;
+                                    ;;
+            -h | --help )           usage;
+                                    exit;
+                                    ;;
+            * )                     usage;
+                                    exit;
                                     ;;
         esac
-        shift
+        shift;
     done
     
 else
     usage;
+    exit;
 fi
 
 #
@@ -53,11 +63,41 @@ fi
 
 # check if name already exists
 if [[ ! -e $PROJECT_NAME ]]; then
-    printf "Directory does not exist yet, creating...\n"
+    printf "Project does not exist yet, creating project directory structure...\n"
     mkdir $PROJECT_NAME;
-    cd $PROJECT_NAME;
-    printf "Entered new directory, creating default structure...\n"
+    mkdir $PROJECT_NAME/cmake;
+    mkdir $PROJECT_NAME/include;
+    mkdir $PROJECT_NAME/include/$PROJECT_NAME;
+    mkdir $PROJECT_NAME/src;
+    mkdir $PROJECT_NAME/apps;
+    mkdir $PROJECT_NAME/tests;
+    mkdir $PROJECT_NAME/docs;
+    mkdir $PROJECT_NAME/extern;
+    mkdir $PROJECT_NAME/scripts;
 
+    printf "Directory structure created, touching files...\n"
+    touch $PROJECT_NAME/CMakeLists.txt;
+    touch $PROJECT_NAME/src/CMakeLists.txt;
+    touch $PROJECT_NAME/apps/CMakeLists.txt;
+
+    # write top level cmake boilerplate
+    if [ "$CREATE_CMAKE_BOILERPLATE" = true ] ; then
+        cd $PROJECT_NAME;
+        echo "cmake_minimum_required(VERSION $CMAKE_VERSION)" >> CMakeLists.txt;
+        echo "project($PROJECT_NAME VERSION 1.0)" >> CMakeLists.txt;
+        echo "set(CMAKE_MODULE_PATH \"\${PROJECT_SOURCE_DIR}/cmake\" \${CMAKE_MODULE_PATH})" >> CMakeLists.txt;
+        cd ..;
+    fi
+
+    # do the git repo last, so all things are included
+    if [ "$CREATE_GIT_REPOSITORY" = true ] ; then
+        cd $PROJECT_NAME;
+        echo "/build*" >> .gitignore;
+        git init;
+        git add .;
+        git commit -m "Created CMake project structure."
+        cd ..;
+    fi;
 else
     printf "A file or directory named \"$PROJECT_NAME\" already exists. Please specify an unused name."
 fi
